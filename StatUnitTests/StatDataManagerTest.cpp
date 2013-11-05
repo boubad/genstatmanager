516db9f75c0ff70311b0692ea8a3ef5def56de96
Boubacar Diarra <boubadiarra@gmail.com>
2013-11-05 23:27:49 +0100

    added statdatamanager testcase

--- StatUnitTests/StatDataManagerTest.cpp
+++ StatUnitTests/StatDataManagerTest.cpp
@@ -14,14 +14,35 @@ namespace StatUnitTests
 		TEST_CLASS_INITIALIZE(ClassInitialize)
 		{
 			Logger::WriteMessage("In Class Initialize");
+			std::string databasename(TEST_DATABASE_FILENAME);
+			m_man.reset(new PredStatDataManagerType());
+			PredStatDataManagerType *pMan = m_man.get();
+			Assert::IsNotNull(pMan);
+			bool bRet = pMan->open(databasename);
+			Assert::IsTrue(bRet);
+			Assert::IsTrue(pMan->is_valid());
+			Logger::WriteMessage("");
 		}
 		TEST_CLASS_CLEANUP(ClassCleanup)
 		{
 			Logger::WriteMessage("In Class Cleanup");
+			m_man.reset();
+			Logger::WriteMessage("");
 		}
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TestGetAllDatasets)
 		{
-			// TODO: Ici, votre code de test
+			PredStatDataManagerType *pMan = m_man.get();
+			Assert::IsNotNull(pMan);
+			Assert::IsTrue(pMan->is_valid());
+			std::vector<DatasetType> oVec;
+			bool bRet = pMan->get_all_datasets(oVec);
+			Assert::IsTrue(bRet);
+			size_t n = oVec.size();
+			bRet = (n > 0);
+			Assert::IsTrue(bRet);
+			const DatasetType &oSet = *(oVec.begin());
+			int nId = oSet.id();
+			Assert::IsTrue(nId != 0);
 		}
 
 	};// classStatDataManagerTest
